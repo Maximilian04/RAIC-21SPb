c3b4e56f4f5a51c0a8642b10b9b684ffa034a48c
Maximilian04 <max4tim@bk.ru>
2021-09-29 11:54:29 +0300

    Вывел параметры model::Game в текстовый файл

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -1,9 +1,38 @@
 #include "MyStrategy.hpp"
 #include <exception>
 
+#include <vector>
+#include <iostream>
+using namespace std;
+
 MyStrategy::MyStrategy() {}
 
-model::Action MyStrategy::getAction(const model::Game& game)
-{
-    return model::Action(std::vector<model::MoveAction>(), std::vector<model::BuildingAction>());
+model::Action MyStrategy::getAction(const model::Game& game) {
+	vector<int> targetPlanets;
+	vector<model::MoveAction> moveActions;
+	vector<model::BuildingAction> buildingActions;
+
+	for (int i = 0; i < game.planets.size(); ++i) {
+		if (game.planets[i].harvestableResource.has_value()) {
+			targetPlanets.push_back(i);
+
+			buildingActions.push_back(model::BuildingAction(i, optional<model::BuildingType>(
+					(model::BuildingType) (int) game.planets[i].harvestableResource.value())));
+		}
+	}
+
+	for (int i = 0; i < game.planets.size(); ++i) {
+		for (model::WorkerGroup workerGroup: game.planets[i].workerGroups) {
+			if (workerGroup.playerIndex == game.myIndex) {
+				for (int targetPlanet: targetPlanets) {
+					moveActions.push_back(model::MoveAction(i, targetPlanet, workerGroup.number / targetPlanets.size(),
+															optional<model::Resource>(model::Resource::STONE)));
+				}
+			}
+		}
+	}
+
+	cout << game.toString() << endl;
+
+	return model::Action(moveActions, buildingActions);
 }
