7644ad11c1190f7eeb044ac22ad9c894a0a27834
DangerousVegetable <mukoseev.l@yandex.ru>
2021-11-03 16:02:46 +0300

    Улучшаем систему по выборе плана застройки сданий.

--- MyStrategy.hpp
+++ MyStrategy.hpp
@@ -10,6 +10,11 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <random>
+#include <algorithm>
+#include <chrono>
+#include <map>
+#include <set>
 
 using namespace std;
 
@@ -19,10 +24,16 @@ public:
     model::Action getAction(const model::Game& game);
 
 	int homePlanet;
+	vector<int> enemyhomePlanets;
 	vector<Planet> planetInf;
 	vector<vector<int>> planetDists;
 	void init(const model::Game& game);
 	void separatePlanets(const model::Game& game); // generating list of planets
+
+	bool onMySide(int id);
+	//int buildeff(vector<int> candidates);
+	//int planetType(const model::Game& game, int id);
+	//bool compareBuilds(vector<int> v1, vector<int> v2);
 
 	int resetTimer;
 	Cycle prodCycle;
