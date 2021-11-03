198f330c12fdd93d2d6ee75182767dfd5ee81fc6
Максимилиан <33744661+Maximilian04@users.noreply.github.com>
2021-11-03 18:13:09 +0300

    CodeStyle в MyStrategy.cpp

--- MyStrategy.cpp
+++ MyStrategy.cpp
@@ -3,41 +3,18 @@
 
 MyStrategy::MyStrategy() : homePlanet(-1), resetTimer(0) {}
 
-int planetType(const model::Game& game, int id)
-{
-	if (game.planets[id].harvestableResource.has_value() && game.planets[id].harvestableResource.value() != t2r(STONE)) //has a harvestable resource differing from stone
-	{
-		int resource = r2t(game.planets[id].harvestableResource.value());
-		return resource;
-	}
-	else return 3;
+int planetType(const model::Game& game, int id) {
+	if (game.planets[id].harvestableResource.has_value() &&
+		game.planets[id].harvestableResource.value() != t2r(STONE)) { //has a harvestable resource differing from stone
+		return r2t(game.planets[id].harvestableResource.value());
+	} else return 3;
 }
 
-/*bool compareBuilds(const MyStrategy* strat, vector<int> b1, vector<int> b2)
-{
-	return buildeff(strat, b1) < buildeff(strat, b2);
-}*/
-
-//
-
-int buildeff(const MyStrategy* strat, vector<int> candidates)
-{
-	/*const int coeff[10][10] = {{0,0,0,8,0,0,0,0,0,8},
-	{0,0,0,0,8,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,0,0,0,0,0,0,0}}; */
+int buildeff(const MyStrategy* strat, vector<int> candidates) {
 	static int coeff[10][10];
-	if(coeff[MINES][FOUNDRY] != 8) //coeff initialization
-	{
-		for(int i = 0; i < 10; i++)
-		{
-			for(int j = 0; j < 10; j++) coeff[i][j] = 0;
+	if (coeff[MINES][FOUNDRY] != 8) { //coeff initialization
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) coeff[i][j] = 0;
 		}
 
 		coeff[MINES][FOUNDRY] = 8;
@@ -45,7 +22,6 @@ int buildeff(const MyStrategy* strat, vector<int> candidates)
 
 		coeff[MINES][EXTRAFOUNDRY] = 8;
 		coeff[EXTRAFOUNDRY][MINES] = 4;
-
 
 		coeff[FOUNDRY][CHIP_FACTORY] = 2;
 		coeff[EXTRAFOUNDRY][CHIP_FACTORY] = 2;
@@ -75,43 +51,36 @@ int buildeff(const MyStrategy* strat, vector<int> candidates)
 
 		coeff[BIOREACTOR][ACCUMULATOR_FACTORY] = 2;
 	}
-	
+
 	int ans = 0;
-	for(int i = 0; i < 10; i++)
-	{
-		for(int j = 0; j < 10; j++)
-		{
-			ans += coeff[i][j]*(strat->planetDists)[candidates[i]][candidates[j]];
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			ans += coeff[i][j] * (strat->planetDists)[candidates[i]][candidates[j]];
 		}
 	}
 
 	return ans;
 }
 
-struct BuildComp
-{
+struct BuildComp {
 	MyStrategy* strat;
-	BuildComp(MyStrategy* s)
-	{
+	BuildComp(MyStrategy* s) {
 		strat = s;
 	}
 
-	bool operator() (vector<int> b1, vector<int> b2)
-	{
+	bool operator()(vector<int> b1, vector<int> b2) {
 		return buildeff(strat, b1) < buildeff(strat, b2);
 	}
 };
 
-bool MyStrategy::onMySide(int id)
-{	
+bool MyStrategy::onMySide(int id) {
 	int dist = -1;
-	for(int en: enemyhomePlanets)
-	{
-		if(dist == -1) dist = planetDists[id][en];
+	for (int en: enemyhomePlanets) {
+		if (dist == -1) dist = planetDists[id][en];
 		else dist = min(dist, planetDists[id][en]);
 	}
 
-	return ((double)dist > 0.75*(double)planetDists[id][homePlanet]);
+	return ((double) dist > 0.75 * (double) planetDists[id][homePlanet]);
 }
 
 model::Action MyStrategy::getAction(const model::Game& game) {
@@ -119,13 +88,12 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 	separatePlanets(game);
 	++resetTimer;
 
-
 	vector<model::MoveAction> moveActions;
 	vector<model::BuildingAction> buildActions;
 
 	if (!prodCycle.isPlanned) {
 		cout << "Calculating optimal building..." << "\n";
-		
+
 		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 		const int attepmts = 175;
 		const int opts = 100;
@@ -134,71 +102,65 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 
 		vector<vector<int>> ptypes; //planet type: planets' ids
 		ptypes.resize(5); // 0-2 - RES, 3 - NO RES, 4 - ALL
-		for (int id = 0; id < game.planets.size(); ++id)
-		{
-			if(planetInf[id].planetOwner && !game.planets[id].building.has_value() && onMySide(id)) //is mine, doesn't have a building, and on 'my' side
-			{
+		for (int id = 0; id < game.planets.size(); ++id) {
+			//is mine, doesn't have a building, and on 'my' side
+			if (planetInf[id].planetOwner && !game.planets[id].building.has_value() && onMySide(id)) {
 				ptypes[planetType(game, id)].push_back(id);
 				ptypes[4].push_back(id);
 			}
 		}
 
-		#if 0
+#if 0
 		for(int i = 0; i < 4; i++)
 		{
 			cout << ptypes[i].size() << "\t";
 		}
 		cout << "\n";
-		#endif
+#endif
 
-		vector<int> avdist(5,0); //average distances between planets of one type
-		for(int t = 0; t < 5; t++)
-		{
+		vector<int> avdist(5, 0); //average distances between planets of one type
+		for (int t = 0; t < 5; t++) {
 			int num = 0;
 			int distsum = 0;
-			for(int i = 0; i < ptypes[t].size(); i++)
-			{
-				for(int j = 0; j < ptypes[t].size(); j++)
-				{
+			for (int i = 0; i < ptypes[t].size(); i++) {
+				for (int j = 0; j < ptypes[t].size(); j++) {
 					num++;
 					distsum += planetDists[ptypes[t][i]][ptypes[t][j]];
 				}
 			}
 
-			if(t < 3) avdist[t] = distsum/num;
-			else avdist[t] = game.maxTravelDistance*2.5;
+			if (t < 3) avdist[t] = distsum / num;
+			else avdist[t] = game.maxTravelDistance * 2.5;
 		}
 
-		vector<vector<int>> pgraph(game.planets.size()); //graph where objects are planets connected with their 'neighbour' planets with the same type
-		for(int i = 0; i < game.planets.size(); i++)
-		{
+		vector<vector<int>> pgraph(
+				game.planets.size()); //graph where objects are planets connected with their 'neighbour' planets with the same type
+		for (int i = 0; i < game.planets.size(); i++) {
 			int type = planetType(game, i);
-			if(type == 3) type = 4; //if a planet doesn't have resources then its `neigbours` are planets with any type nearby
-			for(int j = 0; j < ptypes[type].size(); j++)
-			{
-				if(planetDists[i][ptypes[type][j]] <= avdist[type])
-				{
+			if (type == 3)
+				type = 4; //if a planet doesn't have resources then its `neigbours` are planets with any type nearby
+			for (int j = 0; j < ptypes[type].size(); j++) {
+				if (planetDists[i][ptypes[type][j]] <= avdist[type]) {
 					pgraph[i].push_back(ptypes[type][j]);
 				}
 			}
 		}
 
 		std::default_random_engine generator;
-		for(int att = 0; att < attepmts; att++) //making attempts trying to find the best solution
+		for (int att = 0; att < attepmts; att++) //making attempts trying to find the best solution
 		{
-			vector<int> current(10,-1);
+			vector<int> current(10, -1);
 
 			/*std::uniform_int_distribution<int> distribution(1,6);
 			int dice_roll = distribution(generator); */
-			for(int i = 0; i < 10; i++)
-			{
+			for (int i = 0; i < 10; i++) {
 				int btype = 3;
-				if(i < 3) btype = i; //building type
+				if (i < 3) btype = i; //building type
 
 				int randompl = -1;
-				while(std::count(current.begin(), current.begin() + i + 1, randompl) > 0) //check if current contains randompl (if it does - choose another randompl)
-				{
-					std::uniform_int_distribution<int> distribution(0, ptypes[btype].size()-1);
+				//check if current contains randompl (if it does - choose another randompl)
+				while (std::count(current.begin(), current.begin() + i + 1, randompl) > 0) {
+					std::uniform_int_distribution<int> distribution(0, ptypes[btype].size() - 1);
 					int randomind = distribution(generator);
 					randompl = ptypes[btype][randomind];
 				}
@@ -207,50 +169,40 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 
 			int cureff = buildeff(this, current);
 			set<int> usedids;
-			map<int,int> indicies;
-			for(int i = 0; i < 10; i++)
-			{
+			map<int, int> indicies;
+			for (int i = 0; i < 10; i++) {
 				usedids.emplace(current[i]);
 				indicies[current[i]] = i;
 			}
 
-			for(int o = 0; o < opts; o++)
-			{
+			for (int o = 0; o < opts; o++) {
 				bool change = false;
-				for(int i = 0; i < 10; i++)
-				{
-					for(int j = 0; j < pgraph[current[i]].size(); j++)
-					{
+				for (int i = 0; i < 10; i++) {
+					for (int j = 0; j < pgraph[current[i]].size(); j++) {
 						int pl = pgraph[current[i]][j];
 						vector<int> newcand = current;
 						bool swap = false;
-						if(usedids.find(pl) != usedids.end()) //if planet is used then try to swap it
+						if (usedids.find(pl) != usedids.end()) //if planet is used then try to swap it
 						{
-							if(indicies[pl] >= 3 || planetType(game, current[i]) == indicies[pl]) //check if we can swap
+							if (indicies[pl] >= 3 ||
+								planetType(game, current[i]) == indicies[pl]) //check if we can swap
 							{
 								newcand[i] = pl;
 								newcand[indicies[pl]] = current[i];
 								/*indicies[current[i]] = indicies[pl]; 
 								indicies[pl] = i;*/
 								swap = true;
-							}
-							else continue; //if we can't swap just move on
-						}
-						else
-						{
+							} else continue; //if we can't swap just move on
+						} else {
 							newcand[i] = pl;
 							//usedids.erase(current[i]);
 						}
 						int neweff = buildeff(this, newcand);
-						if(neweff < cureff)
-						{
-							if(swap)
-							{
-								indicies[current[i]] = indicies[pl]; 
+						if (neweff < cureff) {
+							if (swap) {
+								indicies[current[i]] = indicies[pl];
 								indicies[pl] = i;
-							}
-							else
-							{
+							} else {
 								usedids.erase(current[i]);
 								usedids.emplace(pl);
 								indicies.erase(current[i]);
@@ -264,7 +216,7 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 						}
 					}
 				}
-				if(!change) break;
+				if (!change) break;
 			}
 			//cout << cureff << "\n";
 
@@ -274,22 +226,21 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 		std::sort(candidates.begin(), candidates.end(), BuildComp(this)); //sorting candidates
 		vector<int> bestbuilding = candidates[0];
 
-		for(int i = 0; i < 10; i++)
-		{
+		for (int i = 0; i < 10; i++) {
 			cout << i << ": " << bestbuilding[i] << "\n";
 		}
 		cout << buildeff(this, bestbuilding) << '\n';
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-		std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "ms" << std::endl;
+		std::cout << "Time difference = " <<
+				  std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "ms" << std::endl;
 
-		for(int i = 0; i < 10; i++)
-		{
+		for (int i = 0; i < 10; i++) {
 			prodCycle.buildingPlanet[i] = bestbuilding[i];
 		}
 
 		prodCycle.isPlanned = true;
-		#if 0
+#if 0
 		for (int id = 0; id < game.planets.size(); ++id) { // перебираем планеты с ресурсами
 			if (game.planets[id].harvestableResource.has_value() &&
 				game.planets[id].harvestableResource.value() != t2r(STONE)) {
@@ -433,7 +384,7 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 		cout << prodCycle.buildingPlanet[EXTRAFOUNDRY] << endl;
 
 		prodCycle.isPlanned = true;
-		#endif
+#endif
 	} else if (!prodCycle.isBuilt) {
 		int freeStone = min(game.planets[homePlanet].resources.count(t2r(STONE)) ?
 							game.planets[homePlanet].resources.at(t2r(STONE)) : 0,
@@ -534,7 +485,7 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 													 {prodCycle.buildingPlanet[REPLICATOR],          1.0 / 8}},
 											 {
 													 {prodCycle.buildingPlanet[MINES], 1.0 / 2}},
-													 16*3)) {
+											 16*3)) {
 
 						prodCycle.stackedPlanet[FOUNDRY] = false;
 					}
@@ -547,7 +498,7 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 													 {prodCycle.buildingPlanet[REPLICATOR],          1.0 / 8}},
 											 {
 													 {prodCycle.buildingPlanet[MINES], 1.0 / 2}},
-													 16*3)) {
+											 16*3)) {
 
 						prodCycle.stackedPlanet[EXTRAFOUNDRY] = false;
 					}
@@ -558,7 +509,7 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 													 {prodCycle.buildingPlanet[CHIP_FACTORY], 1.0 / 2}},
 											 {
 													 {prodCycle.buildingPlanet[CAREER], 1.0 / 2}},
-													 12*3)) {
+											 12*3)) {
 
 						prodCycle.stackedPlanet[FURNACE] = false;
 					}
@@ -569,7 +520,7 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 													 {prodCycle.buildingPlanet[ACCUMULATOR_FACTORY], 1.0 / 2}},
 											 {
 													 {prodCycle.buildingPlanet[FARM], 1.0 / 2}},
-													 12*3)) {
+											 12*3)) {
 
 						prodCycle.stackedPlanet[BIOREACTOR] = false;
 					}
@@ -581,7 +532,7 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 											 {
 													 {prodCycle.buildingPlanet[MINES],  1.0 / 3},
 													 {prodCycle.buildingPlanet[CAREER], 1.0 / 3}},
-													 12*3)) {
+											 12*3)) {
 
 						prodCycle.stackedPlanet[CHIP_FACTORY] = false;
 					}
@@ -593,7 +544,7 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 											 {
 													 {prodCycle.buildingPlanet[MINES], 1.0 / 3},
 													 {prodCycle.buildingPlanet[FARM],  1.0 / 3}},
-													 12*3)) {
+											 12*3)) {
 
 						prodCycle.stackedPlanet[ACCUMULATOR_FACTORY] = false;
 					}
@@ -604,8 +555,8 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 											 {
 													 {prodCycle.buildingPlanet[FARM],   1.0 / 7},
 													 {prodCycle.buildingPlanet[CAREER], 1.0 * 2 / 7},
-													 {prodCycle.buildingPlanet[MINES],   1.0 * 4 / 7}},
-													 14*3)) {
+													 {prodCycle.buildingPlanet[MINES],  1.0 * 4 / 7}},
+											 14*3)) {
 
 						prodCycle.stackedPlanet[REPLICATOR] = false;
 					}
@@ -616,8 +567,8 @@ model::Action MyStrategy::getAction(const model::Game& game) {
 										 {
 												 {prodCycle.buildingPlanet[FARM],   1.0 / 7},
 												 {prodCycle.buildingPlanet[CAREER], 1.0 * 2 / 7},
-												 {prodCycle.buildingPlanet[MINES],   1.0 * 4 / 7}},
-												 0, true);
+												 {prodCycle.buildingPlanet[MINES],  1.0 * 4 / 7}},
+										 0, true);
 				}
 			}
 		}
@@ -662,7 +613,8 @@ void MyStrategy::init(const model::Game& game) {
 			game.planets[i].workerGroups[0].playerIndex == game.myIndex) {
 			homePlanet = i;
 		}
-		if (!game.planets[i].workerGroups.empty() && game.planets[i].workerGroups[0].playerIndex != game.myIndex) {
+		if (!game.planets[i].workerGroups.empty() &&
+			game.planets[i].workerGroups[0].playerIndex != game.myIndex) {
 			enemyhomePlanets.push_back(i);
 		}
 	}
